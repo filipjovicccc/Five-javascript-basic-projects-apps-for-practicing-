bf307ec0727d5cfca8af9bcd51bd3ba40de16e8e
filipjovicccc <filipjovicccc@gmail.com>
2021-09-14 17:51:22 +0200

    update

--- memory game/main.js
+++ memory game/main.js
@@ -1,165 +1,70 @@
-let boxOne = document.querySelector("#wrap1");
+jQuery(document).ready(function ($) {
+    var wrapp = $('#wrapp');
+    wrapp.append('<div class="container"></div>');
+    var container = $('.container');
+    var itemAr = [1, 2, 3, 4, 5, 6, 7, 8, 1, 2, 3, 4, 5, 6, 7, 8];
+    var testCliks = 0;
+    var savedAr = [];
+    var kraj = 0;
+    var vreme = 30;
 
-let boxTwo = document.querySelector("#wrap2");
+    for (var i = 0; i < 16; i++) {
+        var rand = Math.floor(Math.random() * itemAr.length);
+        container.append('<div class="box"><div class="back">' + itemAr[rand] + '</div><div class="front"></div><div>');
+        itemAr.splice(rand, 1);
+    };
 
-let boxThree = document.querySelector("#wrap3");
+    var boxes = $('.box');
+    function timeLeft() {
+        var a = setInterval(function () {
+            vreme--;
+            if (vreme === 0) {
+                alert('Vreme je isteklo');
+                boxes.off();
+                clearInterval(a);
+                $('.timers').text('GAME OVER');
+            } else {
+                $('.timers').text('Ostalo je jos ' + vreme + ' sekundi');
+            }
+        }, 1000)
+    }
+    timeLeft();
 
-let boxFour = document.querySelector("#wrap4");
+    //
 
-let boxFive = document.querySelector("#wrap5");
-
-let boxSix = document.querySelector("#wrap6");
-
-let boxSeven = document.querySelector("#wrap7");
-
-let boxEight = document.querySelector("#wrap8");
-
-
-let imageOne = document.querySelector("#myImg1");
-
-let imageTwo = document.querySelector("#myImg2");
-
-let imageThree = document.querySelector("#myImg3");
-
-let imageFour = document.querySelector("#myImg4");
-
-let imageFive = document.querySelector("#myImg5");
-
-let imageSix = document.querySelector("#myImg6");
-
-let imageSeven = document.querySelector("#myImg7");
-
-let imageEight = document.querySelector("#myImg8");
-
-
-// for (let i = 1; i < open.length; i++) {
-
-//     let open = document.querySelectorAll(".open");
-
-//     console.log(open)
-
-//     open.addEventListener("click", function () {
-
-//         if (imageOne.style.display !== "none") {
-//             imageOne.style.display = "none";
-
-//         } else {
-//             imageOne.style.display = "block"
-//         }
-
-//     })
-
-
-// }
+    start();
+    function start() {
+        boxes.click(function () {
+            savedAr.push($(this));
+            testCliks++;
+            $(this).find('.front').css('transform', 'perspective(900px) rotateY(180deg)');
+            $(this).find('.back').css('transform', 'perspective(900px) rotateY(0deg)');
+            if (testCliks === 2) {
+                boxes.off();
+                if (savedAr[0].html() === savedAr[1].html()) {
+                    testCliks = 0;
+                    savedAr.length = 0;
+                    kraj++;
+                    if (kraj === 8) {
+                        alert('igra je zavrsena')
+                    };
+                    start();
+                } else {
+                    setTimeout(function () {
+                        savedAr[0].find('.front').css('transform', 'perspective(900px) rotateY(0deg)');
+                        savedAr[0].find('.back').css('transform', 'perspective(900px) rotateY(180deg)');
+                        savedAr[1].find('.front').css('transform', 'perspective(900px) rotateY(0deg)');
+                        savedAr[1].find('.back').css('transform', 'perspective(900px) rotateY(180deg)');
+                        testCliks = 0;
+                        savedAr.length = 0;
+                        start();
+                    }, 800)
 
 
+                }
+            };
 
-boxOne.addEventListener("click", function (event) {
-
-    if (imageOne.style.display !== "none") {
-        imageOne.style.display = "none";
-
-    } else {
-        imageOne.style.display = "block"
+        });
     }
 
-})
-
-console.log(boxOne.innerHTML)
-
-
-
-boxTwo.addEventListener("click", function () {
-
-    if (imageTwo.style.display !== "none") {
-        imageTwo.style.display = "none";
-
-    } else {
-        imageTwo.style.display = "block"
-    }
-
-})
-
-boxThree.addEventListener("click", function () {
-
-    if (imageThree.style.display !== "none") {
-        imageThree.style.display = "none";
-
-    } else {
-        imageThree.style.display = "block"
-    }
-
-})
-
-boxFour.addEventListener("click", function () {
-
-    if (imageFour.style.display !== "none") {
-        imageFour.style.display = "none";
-
-    } else {
-        imageFour.style.display = "block"
-    }
-
-})
-
-boxFive.addEventListener("click", function () {
-
-    if (imageFive.style.display !== "none") {
-        imageFive.style.display = "none";
-
-    } else {
-        imageFive.style.display = "block"
-    }
-
-})
-
-boxSix.addEventListener("click", function () {
-
-    if (imageSix.style.display !== "none") {
-        imageSix.style.display = "none";
-
-    } else {
-        imageSix.style.display = "block"
-    }
-
-})
-
-boxSeven.addEventListener("click", function () {
-
-    if (imageSeven.style.display !== "none") {
-        imageSeven.style.display = "none";
-
-    } else {
-        imageSeven.style.display = "block"
-    }
-
-})
-
-boxEight.addEventListener("click", function () {
-
-    if (imageEight.style.display !== "none") {
-        imageEight.style.display = "none";
-
-    } else {
-        imageEight.style.display = "block"
-    }
-
-})
-
-
-
-
-// let test = imageOne.style.display
-// console.log(test)
-
-// if ((imageOne.style.display && imageFive.style.display !== "none")) {
-
-//     console.log("acasc")
-// }
-
-
-
-
-
-
-
+});
